89bbbb7c28bfd1a908ec919ae85e84e90e59a465
Mexriddin <maxkamtayev@gmail.com>
2023-12-17 18:38:18 +0500

    first test

---
+++ conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture(scope="session", autouse=True)
+def driver():
+    options = webdriver.ChromeOptions()
+    # options.add_argument("--headless")
+    options.add_argument("window-size=1920x1080")
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
